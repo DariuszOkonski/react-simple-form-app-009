165078d5e6a26818b13f2b6143d8a822beefe36c
Dariusz Okonski <darek200180@gmail.com>
2019-04-26 01:15:02 +0100

    Updates

---
+++ precache-manifest.7ef17fafcb4126ae046e3176afac7271.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb0779e8c088c5229103a189e5354f68",
+    "url": "/react-simple-form-app-009/index.html"
+  },
+  {
+    "revision": "bb9b70e4f3e01fad91f0",
+    "url": "/react-simple-form-app-009/static/css/main.6335b775.chunk.css"
+  },
+  {
+    "revision": "774d5e3722e17f42d013",
+    "url": "/react-simple-form-app-009/static/js/2.cba05053.chunk.js"
+  },
+  {
+    "revision": "bb9b70e4f3e01fad91f0",
+    "url": "/react-simple-form-app-009/static/js/main.e85eeb50.chunk.js"
+  },
+  {
+    "revision": "c24b3f8e5229cfca493f",
+    "url": "/react-simple-form-app-009/static/js/runtime~main.c9d991c2.js"
+  }
+]);
